74110e3a67a1ac5573e025eca29accbe751a9348
A-Prudnikova <a.prudnikova@gmail.com>
2021-03-18 12:58:12 +0300

    test update-2

--- src/test/java/tests/RegFormTests.java
+++ src/test/java/tests/RegFormTests.java
@@ -27,18 +27,30 @@ public class RegFormTests {
         $("#lastName").setValue("Yyy");
         $("#userEmail").setValue("Yyy@aaa.ru");
         $(byText("Male")).click();
+        $(byText("Female")).click();
+        $(byText("Other")).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("0");
         $(".react-datepicker__year-select").selectOptionByValue("1910");
         $(".react-datepicker__day--003").click();
-        $("#currentAddress").setValue("Hell");
+        $("#subjectsInput").click();
+        $("#subjectsInput").setValue("M").pressEnter();
+        $("#subjectsInput").setValue("s").pressEnter();
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
         File file = new File("src/test/resourses/readme.txt");
         $("#uploadPicture").uploadFile(file);
+        $("#currentAddress").setValue("Street N");
+        String state = "NCR";
+        $("#state").click();
+        $(byText(state)).click();
+        String city = "Delhi";
+        $("#city").click();
+        $(byText(city)).click();
         $("#submit").click();
         $(".modal-content").shouldBe(Condition.visible).shouldHave(text("Ana"));
-        sleep(4000);
-        System.out.println("wow");
 
     }
 }
