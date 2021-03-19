69010acb63bc2865761b137ec36c4c4d550eb387
A-Prudnikova <a.prudnikova@gmail.com>
2021-03-19 11:30:41 +0300

    test update-6

--- src/test/java/tests/RegFormTests.java
+++ src/test/java/tests/RegFormTests.java
@@ -1,14 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,41 +19,48 @@ public class RegFormTests {
 
     @Test
     void successfulFillTest() {
+        String name = "Ana";
+        String surname = "Yyy";
+        String email = "Yyy@aaa.ru";
+        String phone = "1234567890";
+        String subject = "English";
+        String address = "Street N build.123";
+        String state = "NCR";
+        String city = "Delhi";
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Ana");
-        $("#lastName").setValue("Yyy");
-        $("#userEmail").setValue("Yyy@aaa.ru");
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(surname);
+        $("#userEmail").setValue(email);
         $(byText("Male")).click();
         $(byText("Female")).click();
         $(byText("Other")).click();
-        $("#userNumber").setValue("1234567890");
+        $("#userNumber").setValue(phone);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("0");
         $(".react-datepicker__year-select").selectOptionByValue("1910");
         $(".react-datepicker__day--003").click();
-        $("#subjectsInput").setValue("M").pressEnter();
-        $("#subjectsInput").setValue("s").pressEnter();
+        $("#subjectsInput").setValue(subject).pressEnter();
         $(byText("Sports")).click();
         $(byText("Reading")).click();
         $(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("readme.txt");
-        $("#currentAddress").setValue("Street N build.123");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#uploadPicture").uploadFromClasspath("ava.jpg");
+        $("#currentAddress").setValue(address);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
         $("#submit").click();
-        $(".modal-content").shouldBe(Condition.visible).shouldHave(
-                text("Ana"),
-                text("Yyy"),
-                text("Yyy@aaa.ru"),
+        $(".modal-content").shouldBe(visible).shouldHave(
+                text(name),
+                text(surname),
+                text(email),
                 text("Other"),
-                text("1234567890"),
+                text(phone),
                 text("03 January,1910"),
-                text("Maths, English"),
+                text(subject),
                 text("Sports, Reading, Music"),
-                text("readme.txt"),
-                text("Street N build.123"),
-                text("NCR Delhi")
+                text("ava.jpg"),
+                text(address),
+                text(state),
+                text(city)
         );
-
     }
 }
